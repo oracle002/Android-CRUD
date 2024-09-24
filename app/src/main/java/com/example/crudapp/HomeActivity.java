71cb3043b13642869f98d918729b42173302224d
Sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-09-24 22:10:27 +0530

    PART 2

--- app/src/main/java/com/example/crudapp/HomeActivity.java
+++ app/src/main/java/com/example/crudapp/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.crudapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class HomeActivity extends AppCompatActivity {
 
+    public Button create,update,delete,read;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +23,35 @@ public class HomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        create = findViewById(R.id.create);
+        update = findViewById(R.id.update);
+        delete = findViewById(R.id.delete);
+        read = findViewById(R.id.read);
+
+        create.setOnClickListener(v ->{
+            //from to
+            Intent intent = new Intent(HomeActivity.this,CreateActivity.class);
+            startActivity(intent);
+        });
+
+        update.setOnClickListener(v ->{
+            //from to
+            Intent intent = new Intent(HomeActivity.this,updateActivity.class);
+            startActivity(intent);
+        });
+
+        delete.setOnClickListener(v ->{
+            //from to
+            Intent intent = new Intent(HomeActivity.this,DeleteActivity.class);
+            startActivity(intent);
+        });
+
+        read.setOnClickListener(v ->{
+            //from to
+            Intent intent = new Intent(HomeActivity.this,ReadActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
