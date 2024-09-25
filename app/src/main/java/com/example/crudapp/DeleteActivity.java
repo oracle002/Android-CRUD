5165bddcbb00d29020c754bda011a67dbcf03753
Sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-09-25 09:48:15 +0530

    Delete and Update

--- app/src/main/java/com/example/crudapp/DeleteActivity.java
+++ app/src/main/java/com/example/crudapp/DeleteActivity.java
@@ -1,6 +1,9 @@
 package com.example.crudapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class DeleteActivity extends AppCompatActivity {
+
+    DbHelper dbHelper;
+    EditText deleteitem;
+    Button deletebtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,32 @@ public class DeleteActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        dbHelper = new DbHelper(this);
+        deleteitem = findViewById(R.id.deleteitem);
+        deletebtn = findViewById(R.id.deletebtn);
+
+        deletebtn.setOnClickListener(v ->{
+            //get string, check empty, if not empty call deleleteitem function, toast, clear input
+            String item = deleteitem.getText().toString().trim();
+
+            if(item.isEmpty()){
+                Toast.makeText(DeleteActivity.this, "Please fill the item name", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            if(dbHelper.deleteItem(item)){
+                Toast.makeText(DeleteActivity.this, "SUCCESS", Toast.LENGTH_LONG).show();
+                finish();
+            }
+            else{
+                Toast.makeText(DeleteActivity.this, "FAILED", Toast.LENGTH_LONG).show();
+                deleteitem.setText("");
+            }
+
+
+        });
+
+
     }
 }
--- app/src/main/java/com/example/crudapp/updateActivity.java
+++ app/src/main/java/com/example/crudapp/updateActivity.java
@@ -1,6 +1,9 @@
 package com.example.crudapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class updateActivity extends AppCompatActivity {
 
+    DbHelper dbHelper;
+    EditText itemfield, qntyfeild;
+    Button updatebtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,37 @@ public class updateActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        dbHelper = new DbHelper(this);
+        itemfield = findViewById(R.id.updateitem);
+        qntyfeild = findViewById(R.id.updateqnty);
+        updatebtn = findViewById(R.id.updatebtn);
+
+
+        updatebtn.setOnClickListener(v ->{
+            //get both fields, check empty, if not empty - > updateitem pass values
+
+            String item = itemfield.getText().toString().trim();
+            String quantity = qntyfeild.getText().toString().trim();
+
+            if( item.isEmpty() || quantity.isEmpty()){
+
+                Toast.makeText(updateActivity.this, "PLEASE FILL ALL THE FIELDS" , Toast.LENGTH_LONG).show();
+                return;
+            }
+            int quant = Integer.parseInt(quantity);
+            if( dbHelper.updateItem(item,quant)){
+
+                Toast.makeText(updateActivity.this, "SUCCESS", Toast.LENGTH_LONG).show();
+                finish();
+            }
+            else{
+                Toast.makeText(updateActivity.this, "FAILED", Toast.LENGTH_LONG).show();
+                itemfield.setText("");
+                qntyfeild.setText("");
+            }
+        });
+
+
     }
 }
